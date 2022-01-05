a7e9f3ca642b9168b91f1c1e8e952e7f1a819102
Chris Boehm <dogboydog@users.noreply.github.com>
2022-01-04 19:55:31 -0500

    initial commit

---
+++ Assets/MyYarnCommand.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class MyYarnCommand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [YarnCommand(name: "HelloWorld")]
+    public void HelloWorldCommand(string worldString)
+    {
+        Debug.Log($"World string {worldString}");
+    }
+}
